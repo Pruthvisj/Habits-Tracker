e37838315158eda5b9b912087e633581b3a57a01
Pruthvisj <89569955+Pruthvisj@users.noreply.github.com>
2025-05-09 19:37:14 +0530

    Component: Created list to track the streaks.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import './App.css'
 import AddHabitForm from './components/add-habit-form'
 import store from './store/store'
+import HabitList from './components/habit-list'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Container maxWidth="md">
         <Typography component="h1" variant='h2' align='center'> Habit Tracket </Typography>
         <AddHabitForm/>
+        <HabitList/>
       </Container>
     </Provider>
   )
--- src/store/habit-slice.ts
+++ src/store/habit-slice.ts
@@ -33,8 +33,26 @@ const habitSlice = createSlice({
 
             state.habits.push(newHabit);
         },
-    }
+        toggleHabit: (
+            state, action: PayloadAction<{ id:string, date: string }>
+        ) => {
+            const habit = state.habits.find((h) => h.id === action.payload.id)
+            if (habit) {
+                const index = habit.completedDates.indexOf(action.payload.date)
+                if (index > -1) {
+                    habit.completedDates.splice(index, 1)
+                } else {
+                    habit.completedDates.push(action.payload.date)
+                }
+            }
+        },
+        removeHabit: (
+            //here comes state and action with obj
+        ) => {
+            //logic
+        },
+    },
 })
 
-export const {addHabit} = habitSlice.actions;
+export const {addHabit, toggleHabit} = habitSlice.actions;
 export default habitSlice.reducer;
