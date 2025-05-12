77a34c81d0eb642051dd7d10641e63f0c6ca08f3
Pruthvisj <89569955+Pruthvisj@users.noreply.github.com>
2025-05-12 17:47:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Provider store={store}>
       <Container maxWidth="md">
-        <Typography component="h1" variant='h2' align='center'> Habit Tracket </Typography>
+        <Typography component="h1" variant='h2' align='center'> Habit Tracker </Typography>
         <AddHabitForm/>
         <HabitList/>
         <HabitStats/>
